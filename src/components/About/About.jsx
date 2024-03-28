7f665a49106c98fc1de6d42c6482b7ce1de26d21
Camille Mallet <mallet.camille11@gmail.com>
2024-03-28 19:42:08 +0100

    style : About..jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,16 +11,18 @@ function About() {
                     <img className='imgProfile' src={imgProfile}></img>
                     <p className='pProfile'>
                         Passionnée par le développement front-end et la création d'interfaces utilisateurs intuitives,<br/>
-                        <br/>
-                        J'ai débuté mon parcours dans le monde du web.<br/> 
-                        Grâce à une formation intensive de 9 mois chez OpenClassrooms,<br/> 
-                        j'ai acquis une solide expertise en technologies telles que <span className='spanProfile'>React</span>, <span className='spanProfile'>Sass</span>, <span className='spanProfile'>Node.js</span> et <span className='spanProfile'>Redux</span>,<br/> 
+                        J'ai débuté mon parcours dans le monde du web.
+                        Grâce à une formation intensive de 9 mois chez OpenClassrooms, 
+                        j'ai acquis une solide expertise en technologies telles 
+                        que <span className='spanProfile'>React</span>, <span className='spanProfile'>Sass</span>, 
+                        <span className='spanProfile'> Node.js</span> et <span className='spanProfile'>Redux</span>, 
                         me permettant ainsi de concevoir des applications web performantes.<br/> 
                         <br/>
-                        Toujours avide d'apprendre et de collaborer sur de nouveaux projets,<br/> 
+                        Toujours avide d'apprendre et de collaborer sur de nouveaux projets, 
                         je suis constamment à la recherche de défis stimulants et de collaborations enrichissantes.<br/>
                         <br/>
-                        N'hésitez pas à explorer mon portfolio pour découvrir mon travail et à me contacter si vous souhaitez<br/> 
+                        N'hésitez pas à explorer mon portfolio pour découvrir mon travail 
+                        et à me contacter si vous souhaitez 
                         discuter de projets ou simplement échanger.<br/>
                     </p>
                 </div>
