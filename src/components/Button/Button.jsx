2597d3690e1ca57d45d1a3ce071cca4ec22cd4e7
Camille Mallet <mallet.camille11@gmail.com>
2024-03-29 17:15:53 +0100

    add : minor adjustments

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './Button.css';
 
 function Button() {
-    function gohome() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            setIsVisible(scrollTop > 0);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    function goHome() {
         document.body.scrollTop = 0; 
         document.documentElement.scrollTop = 0; 
     }
 
     return (
-        <button className='button' onClick={gohome}>
+        <button className='button' onClick={goHome} style={{ display: isVisible ? 'block' : 'none' }}>
             <i className="button_icon fa-solid fa-chevron-up"></i>
         </button>
     );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer() {
     return (
         <footer className="footer"> 
-            <p className="footer-text">Copyright 2024 Camille.M</p>
+            <p className="footer_text">Copyright 2024 Camille.M</p>
         </footer>
     );
   }
